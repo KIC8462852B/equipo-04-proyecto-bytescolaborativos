7a55284e89f8d83ecc1118087d769300720c20cc
KIC8462852B <lurdanhuszar@protonmail.ch>
2025-11-08 18:17:12 +0100

    crear estructura base para proyecto Spring Boot

---
+++ motorRecomendaciones/src/main/java/com/motorRecomendacionesAPI/motorRecomendaciones/MotorRecomendacionesApplication.java
@@ -0,0 +1,13 @@
+package com.motorRecomendacionesAPI.motorRecomendaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorRecomendacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorRecomendacionesApplication.class, args);
+	}
+
+}
---
+++ motorRecomendaciones/src/test/java/com/motorRecomendacionesAPI/motorRecomendaciones/MotorRecomendacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motorRecomendacionesAPI.motorRecomendaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorRecomendacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
