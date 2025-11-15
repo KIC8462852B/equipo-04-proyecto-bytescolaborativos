03859cf0a716b4a7ed700d14ffd85ce62f816fc0
jose alberto cruz martinez <albertocruz8133@proton.me>
2025-11-15 16:42:07 -0600

    feat(auth): Agrega configuracion de beans para Spring security
    - Agrega Bean para 'UserDetailsService' usando de momento para pruebas
    una instancia de 'InMemoryUserDetailsServiceImpl'
    - Agrega Bean para 'PasswordEncoder' devolviendo una instancia de
    'BCryptPasswordEncoder'
    - Agrega Bean para 'AuthenticationProvider' devolviendo una instancia de
    'DaoAuthenticationProvider' con los beans anteriores de
    'UserDetailsService' y 'PasswordEncoder'
    - Agrega Bean para 'AuthenticationManager' usando el bean de
    'AuthenticationProvider'
    - Agrega Bean para 'SecurityFilterChain' configurando el CSRF, el tipo
    de sesion como STATELESS, endpoints publicos '/auth', bloqueo de
    endpoints para usuarios no autenticados y filtros personalizados para
    JWT

---
+++ motorRecomendaciones/src/main/java/com/motorRecomendacionesAPI/motorRecomendaciones/config/security/SecurityConfiguration.java
@@ -0,0 +1,80 @@
+package com.motorRecomendacionesAPI.motorRecomendaciones.config.security;
+
+import com.motorRecomendacionesAPI.motorRecomendaciones.config.security.filter.JwtAuthenticationFilter;
+import com.motorRecomendacionesAPI.motorRecomendaciones.config.security.filter.JwtAuthorizationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(http -> {
+                    http.requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll();
+                    http.anyRequest().authenticated();
+                })
+                .addFilter(jwtAuthenticationFilter)
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity, AuthenticationProvider authenticationProvider) throws Exception {
+        return httpSecurity
+                .getSharedObject(AuthenticationManagerBuilder.class)
+                .authenticationProvider(authenticationProvider)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.builder()
+                .username("example.user")
+                .password("1234")
+                .roles("ROLE_TEST_USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+}
