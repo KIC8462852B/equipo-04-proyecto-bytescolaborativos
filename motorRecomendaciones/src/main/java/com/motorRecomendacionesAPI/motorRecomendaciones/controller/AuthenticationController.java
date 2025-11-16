dfd98c9f2f9c9967ba6906037b7a5c8d8f271414
jose alberto cruz martinez <albertocruz8133@proton.me>
2025-11-16 14:48:37 -0600

    fix(auth): Actualiza el tipo de DTO en el endpoint de registro y cambia el nombre de los parametros a 'request'

--- motorRecomendaciones/src/main/java/com/motorRecomendacionesAPI/motorRecomendaciones/controller/AuthenticationController.java
+++ motorRecomendaciones/src/main/java/com/motorRecomendacionesAPI/motorRecomendaciones/controller/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.motorRecomendacionesAPI.motorRecomendaciones.controller;
 
 import com.motorRecomendacionesAPI.motorRecomendaciones.dto.AuthenticationRequest;
 import com.motorRecomendacionesAPI.motorRecomendaciones.dto.AuthenticationResponse;
+import com.motorRecomendacionesAPI.motorRecomendaciones.dto.RegisterRequest;
 import com.motorRecomendacionesAPI.motorRecomendaciones.service.interfaces.AuthenticationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,14 +20,14 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody @Valid AuthenticationRequest authenticationRequest) {
-        AuthenticationResponse response = authenticationService.authenticate(authenticationRequest);
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody @Valid AuthenticationRequest request) {
+        AuthenticationResponse response = authenticationService.authenticate(request);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(AuthenticationRequest authenticationRequest) {
-        AuthenticationResponse response = authenticationService.register(authenticationRequest);
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody @Valid RegisterRequest request) {
+        AuthenticationResponse response = authenticationService.register(request);
         return ResponseEntity.ok(response);
     }
 }
