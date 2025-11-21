a990708b73904b0cc014b8ee4760939fb2a4beb9
José Alberto Cruz Martínez <albertocruz8133@proton.me>
2025-11-20 21:40:31 -0600

    refactor(rating): Cambia la clase de herencia RuntimeException por ResourceNotFoundException para mas claridad y consistencia

--- motorRecomendaciones/src/main/java/com/motorRecomendacionesAPI/motorRecomendaciones/exception/RatingNotFoundException.java
+++ motorRecomendaciones/src/main/java/com/motorRecomendacionesAPI/motorRecomendaciones/exception/RatingNotFoundException.java
@@ -1,6 +1,6 @@
 package com.motorRecomendacionesAPI.motorRecomendaciones.exception;
 
-public class RatingNotFoundException extends RuntimeException {
+public class RatingNotFoundException extends ResourceNotFoundException {
     public RatingNotFoundException(String message) {
         super(message);
     }
