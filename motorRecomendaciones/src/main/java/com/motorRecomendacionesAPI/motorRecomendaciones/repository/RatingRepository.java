0cea6359b14ebf94c1961c33085894c9c94b2a98
José Alberto Cruz Martínez <albertocruz8133@proton.me>
2025-11-20 20:55:47 -0600

    feat(rating): Agrega metodos de existencia y calculo de promedio al repositorio de Rating

--- motorRecomendaciones/src/main/java/com/motorRecomendacionesAPI/motorRecomendaciones/repository/RatingRepository.java
+++ motorRecomendaciones/src/main/java/com/motorRecomendacionesAPI/motorRecomendaciones/repository/RatingRepository.java
@@ -5,6 +5,14 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.motorRecomendacionesAPI.motorRecomendaciones.model.Rating;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RatingRepository extends JpaRepository<Rating, UUID> {
+
+    boolean existsByUserIdAndProductId(UUID userId, UUID productId);
+
+    @Query("SELECT COALESCE(AVG(r.score), 0.0) FROM Rating r WHERE r.product.id = :productId")
+    Double getAverageScoreByProductId(UUID productId);
+
+    boolean existsByProductId(UUID productId);
 }
