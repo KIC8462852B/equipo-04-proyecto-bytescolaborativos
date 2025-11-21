bc33e16fcf1913631ac6c112cce1b3a6792429ec
José Alberto Cruz Martínez <albertocruz8133@proton.me>
2025-11-20 21:46:39 -0600

    fix(rating): Agrega 'final' a la propiedad repository
    Corrige problema de inyeccion de dependencia dado que la propiedad
    repository no estaba siendo incluida en el constructor por falta de la
    palabra reservada 'final'

--- motorRecomendaciones/src/main/java/com/motorRecomendacionesAPI/motorRecomendaciones/service/implementation/ProductServiceImpl.java
+++ motorRecomendaciones/src/main/java/com/motorRecomendacionesAPI/motorRecomendaciones/service/implementation/ProductServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
 
-    public ProductRepository repository;
+    public final ProductRepository repository;
 
     @Override
     @Transactional(readOnly = true)
